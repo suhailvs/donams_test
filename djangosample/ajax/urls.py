c679a3d818509870ff8c0bf3d1530c7422eb5470
suhailvs <suhailvs@gmail.com>
2018-06-05 16:38:33 +0530

    added sidebar. added ajax load

--- djangosample/ajax/urls.py
+++ djangosample/ajax/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('ajx/', views.ajax_showpage, name='ajax_showpage'),
+    path('ajx/add/', views.ajax_adduser, name='ajax_adduser'),
+    
 ]
--- djangosample/ajax/views.py
+++ djangosample/ajax/views.py
@@ -3,6 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-
 def index(request):
-    return render(request,"add_user.html",{})
+    return render(request,"home.html",{'page':'users'})
+
+def ajax_showpage(request):
+    curpage=request.GET['page']
+    context = {'page':curpage}
+    if curpage=='users':
+        #nts=Notes.objects.filter(user=request.user).order_by('-created')
+        template='users.html'
+        context['users']=['suhail','sumee','vahee']
+    elif curpage=='add':
+        template = 'add_user.html'
+    
+    return render(request,template,context)
+
+def ajax_adduser(request):
+    if request.method=="POST":
+        for key,value in request.POST.items():
+            print (key, value) 
+    return HttpResponse('hai')
+
