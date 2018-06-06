b40ef7d3776f62508091c863bb3149eaf3d716ff
suhailvs <suhailvs@gmail.com>
2018-06-06 07:09:30 +0530

    added django models

--- djangosample/ajax/models.py
+++ djangosample/ajax/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.forms import ModelForm
+
+class UserDetail(models.Model):
+
+    fullname = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    contact = models.CharField(max_length=50)
+    address1 = models.CharField(max_length=200)
+    address2 = models.CharField(max_length=200)
+
+class UserDetailForm(ModelForm):
+    class Meta:
+        model = UserDetail
+        fields = '__all__'
+
--- djangosample/ajax/views.py
+++ djangosample/ajax/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import UserDetail,UserDetailForm
 def index(request):
     return render(request,"home.html",{'page':'users'})
 
@@ -10,9 +10,8 @@ def ajax_showpage(request):
     curpage=request.GET['page']
     context = {'page':curpage}
     if curpage=='users':
-        #nts=Notes.objects.filter(user=request.user).order_by('-created')
         template='users.html'
-        context['users']=['suhail','sumee','vahee']
+        context['users']=UserDetail.objects.all()#['suhail','sumee','vahee']
     elif curpage=='add':
         template = 'add_user.html'
     
@@ -20,6 +19,9 @@ def ajax_showpage(request):
 
 def ajax_adduser(request):
     if request.method=="POST":
+        form = UserDetailForm(request.POST)
+        if form.is_valid():
+            form.save()
         for key,value in request.POST.items():
             print (key, value) 
     return HttpResponse('hai')
